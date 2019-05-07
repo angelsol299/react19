ff34d7f39885a9861bc970e4277741409ef27cdd
angel <angelsol299@hotmail.com>
2019-05-07 19:50:15 +0200

    chat wireframe

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img src="" alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            amigo
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00 pm</span>
+          </div>
+          <div className="text">Nice blog post!</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
